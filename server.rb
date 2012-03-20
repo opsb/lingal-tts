dbbf0a036f9f26e976a2f5299680de6a21c69d52
opsb <oliver@opsb.co.uk>
2012-03-21 00:14:39 +0100

    Bring uris into line with google translate interface

--- server.rb
+++ server.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'open-uri'
 require 'cgi'
 
-get '/tts' do
+get '/' do
   content_type "audio/mpeg"
-  open("http://translate.google.com/translate_tts?tl=es&q=#{CGI.escape(params[:text])}", "User-Agent" => "Mozilla/5.0")
+  open("http://translate.google.com/translate_tts?tl=#{params[:tl]}&q=#{CGI.escape(params[:q])}", "User-Agent" => "Mozilla/5.0")
 end
