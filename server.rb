ea17921ce2b2a45d17906f9166695fcec4bf4e71
opsb <oliver@opsb.co.uk>
2012-03-21 00:20:15 +0100

    Pass all params through to google tts

--- server.rb
+++ server.rb
@@ -4,5 +4,7 @@ require 'cgi'
 
 get '/' do
   content_type "audio/mpeg"
-  open("http://translate.google.com/translate_tts?tl=#{params[:tl]}&q=#{CGI.escape(params[:q])}", "User-Agent" => "Mozilla/5.0")
+  query_string = params.map{ |k,v| "#{k}=#{CGI.escape(v)}" }.join("&")
+  uri = "http://translate.google.com/translate_tts?" + query_string
+  open(uri, "User-Agent" => "Mozilla/5.0")
 end
