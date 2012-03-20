7f01699d23d128fe1828e57cbce775786b9b4347
opsb <oliver@opsb.co.uk>
2012-03-21 00:08:37 +0100

    URI encode the spanish phrase

--- server.rb
+++ server.rb
@@ -1,7 +1,8 @@
 require 'sinatra'
 require 'open-uri'
+require 'cgi'
 
 get '/tts' do
   content_type "audio/mpeg"
-  open("http://translate.google.com/translate_tts?tl=es&q=#{params[:text]}", "User-Agent" => "Mozilla/5.0")
+  open("http://translate.google.com/translate_tts?tl=es&q=#{CGI.escape(params[:text])}", "User-Agent" => "Mozilla/5.0")
 end
