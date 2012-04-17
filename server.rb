b9cd5c15109113ef841bf5e21563f33b95af93f9
opsb <oliver@opsb.co.uk>
2012-04-18 01:29:09 +0200

    Use URI encoder instead of cgi

--- server.rb
+++ server.rb
@@ -6,7 +6,7 @@ require 'cgi'
 
 get '/' do
   content_type "audio/mpeg"
-  query_string = params.map{ |k,v| "#{k}=#{CGI.escape(v)}" }.join("&")
+  query_string = params.map{ |k,v| "#{k}=#{URI.encode(v)}" }.join("&")
   uri = "http://translate.google.com/translate_tts?" + query_string
   open(uri, "User-Agent" => "Mozilla/5.0")
 end
