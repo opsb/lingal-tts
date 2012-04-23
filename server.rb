87d767b376c5ac4adf42ab7df5a0f4755c8a3123
opsb <oliver@opsb.co.uk>
2012-04-24 01:20:35 +0200

    Pass query string through untouched

--- server.rb
+++ server.rb
@@ -10,7 +10,6 @@ end
 
 get '/' do
   content_type "audio/mpeg"
-  query_string = params.map{ |k,v| "#{k}=#{URI.encode(v)}" }.join("&")
-  uri = "http://translate.google.com/translate_tts?" + query_string
+  uri = "http://translate.google.com/translate_tts?" + request.query_string
   open(uri, "User-Agent" => "Mozilla/5.0")
 end
